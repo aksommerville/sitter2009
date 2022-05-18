7fdba9e10e96778c6e86bc578bb13e7f32a6cd80
Andy Sommerville <aksommerville@gmail.com>
2022-05-17 21:02:57 -0400

    Extra mappings for SN30/N30

--- src/drm_alsa_evdev/sitter_evdev_my_device.cpp
+++ src/drm_alsa_evdev/sitter_evdev_my_device.cpp
@@ -1,17 +1,21 @@
 #include "sitter_evdev_internal.h"
 
-/* Xbox 360 joystick.
+/* Xbox 360 joystick, and also SN30/N30 (grumble, grumble)
  */
 static const struct sitter_my_device sitter_my_device_xbox={
   .vid=0x045e,
   .pid=0x028e,
   .nickname="Xbox 360",
-  .buttonc=5,
+  .buttonc=9,
   .buttonv={
     // type code        usage         0,0 srclo srchi
     {EV_KEY,BTN_SOUTH,SITTER_USAGE_JUMP,0,0,1,INT_MAX},
+    {EV_KEY,BTN_EAST, SITTER_USAGE_PICKUP,0,0,1,INT_MAX},
     {EV_KEY,BTN_NORTH,SITTER_USAGE_PICKUP,0,0,1,INT_MAX},
+    {EV_KEY,BTN_WEST, SITTER_USAGE_PICKUP,0,0,1,INT_MAX},
     {EV_KEY,BTN_START,SITTER_USAGE_PAUSE,0,0,1,INT_MAX},
+    {EV_ABS,ABS_X,    SITTER_USAGE_X,0,0,-10000,10000},
+    {EV_ABS,ABS_Y,    SITTER_USAGE_Y,0,0,-10000,10000},
     {EV_ABS,ABS_HAT0X,SITTER_USAGE_X,0,0,-1,1},
     {EV_ABS,ABS_HAT0Y,SITTER_USAGE_Y,0,0,-1,1},
     //BTN_WEST=north
